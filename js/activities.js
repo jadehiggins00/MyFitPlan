66cf8785894b644181a61b868db2dc36fd2731c8
Jade Higgins <jade.higgins@ptools.com>
2023-03-28 11:09:43 +0100

    tasks are now being added to a list once the add button is clicked

--- js/activities.js
+++ js/activities.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskList = document.querySelector('.task-list')
 
         const taskItem = document.createElement('li');
-        taskItem.textContent = taskInput.ariaValueMax;
+        taskItem.textContent = taskInput.value;
         taskList.appendChild(taskItem);
 
         taskInput.value = '';
@@ -46,55 +46,3 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-// // function to add tasks to the current day of the week
-// function addTask() {
-
-//     const taskInput = document.querySelector('#taskInput')
-//     const taskList = document.querySelector('.task-list')
-
-//     const taskItem = document.createElement('li');
-//     taskItem.textContent = taskInput.ariaValueMax;
-//     taskList.appendChild(taskItem);
-
-//     taskInput.value = '';
-
-// }
-
-// //event listener for the Add button - which calls the addTask()
-// const addTaskButton = document.querySelector('#addTaskButton');
-// addTaskButton.addEventListener('click', addTask);
-
-// Define an array of days of the week
-// const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-// // Get references to the navigation buttons and the header tag
-// const leftButton = document.querySelector('button.btn-custom.ml-5');
-// const rightButton = document.querySelector('button.btn-custom.mr-5');
-// const header = document.querySelector('#day');
-
-// // Set the initial day of the week to Monday
-// let currentDayIndex = 1;
-// header.textContent = daysOfWeek[currentDayIndex];
-
-// // Add click event listeners to the navigation buttons
-// leftButton.addEventListener('click', () => {
-//   // Decrement the current day index
-//   currentDayIndex--;
-//   // If the index is less than zero, wrap around to the end of the array
-//   if (currentDayIndex < 0) {
-//     currentDayIndex = daysOfWeek.length - 1;
-//   }
-//   // Update the header with the new day of the week
-//   header.textContent = daysOfWeek[currentDayIndex];
-// });
-
-// rightButton.addEventListener('click', () => {
-//   // Increment the current day index
-//   currentDayIndex++;
-//   // If the index is greater than the length of the array, wrap around to the beginning
-//   if (currentDayIndex >= daysOfWeek.length) {
-//     currentDayIndex = 0;
-//   }
-//   // Update the header with the new day of the week
-//   header.textContent = daysOfWeek[currentDayIndex];
-// });
