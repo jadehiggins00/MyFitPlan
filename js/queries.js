db1703d42c541408c8935c73b3d7f909d2a8c0cd
Jade Higgins <jade.higgins@ptools.com>
2023-04-05 16:01:56 +0100

    insert to mongodb worked!

--- js/queries.js
+++ js/queries.js
@@ -1,4 +1,33 @@
-// connect to mongodb atlas
-const {MongoClient} = require("mongodb");
+import { MongoClient } from '/index';
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    // Rest of your code here
+    const confirmButton = document.querySelector('#insert-swim-btn');
+
+
+    confirmButton.addEventListener('click', async () => {
+        const client = new MongoClient(uri);
+        await client.connect();
+
+        try{
+            const result = await client.db('ActivitiesDB').collection(Activities).insertOne({
+                DayOfWeek: 'Tuesday',
+                Exercise: 'yoga'
+            });
+
+            console.log(`Inserted document with _id: ${result.insertedId}`);
+
+        }finally{
+            await client.close();
+        }
+
+
+
+
+
+    });
+
+  });
 
 
