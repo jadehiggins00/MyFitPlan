113c44ad14a7a3f490b78716e2400d177a40b2a8
jade <jadehiggins2000@gmail.com>
2023-04-26 20:32:05 +0100

    fuck

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,7 @@ import GoalsDelete from "./components/GoalsDelete"
 import Activities from "./components/Activities"
 import AddActivities from "./components/AddActivities"
 
+
 import LeftArrow from './images/LeftArrow.png';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
